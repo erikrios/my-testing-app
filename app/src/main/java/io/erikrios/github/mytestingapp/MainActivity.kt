fbab163aeabfdd7a49e264258e1d9bdf7a79f2bb
Erik Rio Setiawan <erikriosetiawan49@gmail.com>
2020-12-21 10:05:16 +0700

    Handling the OutOfMemoryException error with glide

--- app/src/main/java/io/erikrios/github/mytestingapp/MainActivity.kt
+++ app/src/main/java/io/erikrios/github/mytestingapp/MainActivity.kt
@@ -4,13 +4,16 @@ import android.os.Bundle
 import android.util.Log
 import android.view.View
 import android.widget.Button
+import android.widget.ImageView
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
+import com.bumptech.glide.Glide
 
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var btnSetValue: Button
     private lateinit var tvText: TextView
+    private lateinit var imgPreview: ImageView
 
     private var names = ArrayList<String>()
 
@@ -20,7 +23,9 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
 
         tvText = findViewById(R.id.tv_text)
         btnSetValue = findViewById(R.id.btn_set_value)
+        imgPreview = findViewById(R.id.img_preview)
         btnSetValue.setOnClickListener(this)
+        Glide.with(this).load(R.drawable.fronalpstock_big).into(imgPreview)
 
         names.add("Narendra Wicaksono")
         names.add("Kevin")
